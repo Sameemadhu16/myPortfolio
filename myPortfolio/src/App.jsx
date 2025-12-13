fe5c9b89f0ce311e035b83863f9c8bdb56a85838
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-12-13 21:12:22 +0530

    feat: Refactor portfolio structure and enhance components
    - Implemented routing with BrowserRouter in App.jsx for better navigation.
    - Replaced logo.svg with logo.jpeg in assets and updated references.
    - Enhanced Hero component with animations and improved layout.
    - Revamped Navbar component to include dynamic scrolling effects and mobile responsiveness.
    - Updated Computers component to utilize Three.js for 3D rendering with responsive design.
    - Added SectionWrapper HOC for consistent section animations across the portfolio.
    - Created styles.js for centralized styling management.

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,10 +1,26 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">3D developer portfolio
-        
-      </h1>
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
