c7bc20b9ee0d4850eb1951523407d8673e60d04a
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-12-24 19:39:51 +0530

    feat: Adjust Hero component layout and update Computers position for better responsiveness

--- myPortfolio/src/components/Hero.jsx
+++ myPortfolio/src/components/Hero.jsx
@@ -7,20 +7,21 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[60px] sm:top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-col sm:flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
+        <div className='hidden sm:flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Sameera Madhuranga</span>
+            Hi, I'm <span className='text-[#915EFF]'>Sameera</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I build full-stack web applications using <br className="sm:block hidden" />
-          MERN stack, React, and Spring Boot microservices
+          I build full-stack web applications using 
+          MERN stack, React, and Spring Boot microservices<br className="sm:block hidden" />
+          
           </p>
 {/* <p className={`${styles.heroSubText} mt-2 text-white-100`}>
   Full-stack developer with a strong interest in <br className="sm:block hidden" />
@@ -33,7 +34,7 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-20 sm:bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
