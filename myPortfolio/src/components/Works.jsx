9efa8d00ab9580f37d3c9057d922a957d1d98e9c
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-12-29 20:46:29 +0530

    feat: Update ProjectCard styles for improved readability and consistency

--- myPortfolio/src/components/Works.jsx
+++ myPortfolio/src/components/Works.jsx
@@ -48,15 +48,15 @@ const ProjectCard = ({
         </div>
 
         <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          <h3 className='text-white font-bold text-[22px] leading-8'>{name}</h3>
+          <p className='mt-3 text-secondary text-[13px] leading-[22px] font-light'>{description}</p>
         </div>
 
         <div className='mt-4 flex flex-wrap gap-2'>
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
+              className={`text-[12px] font-semibold ${tag.color}`}
             >
               #{tag.name}
             </p>
